a335e01ddcea5b158785a56108a7a929a83c286a
saklainmostak1 <saklain15-12003@diu.edu.bd>
2025-01-04 19:21:23 +0600

    'push'

--- server.js
+++ server.js
@@ -15,59 +15,6 @@ var mysql = require('mysql');
 
 
 
-
-app.get('/images/products/original/:year/:month/:day/:hour/:minute/:file', (req, res) => {
-  const { year, month, day, hour, minute, file } = req.params;
-  const imageFilePath = path.join(__dirname, `images/products/original/${year}/${month}/${day}/${hour}/${minute}/${file}`);
-
-  // Check if the image file exists
-  if (fs.existsSync(imageFilePath)) {
-    res.sendFile(imageFilePath);
-  } else {
-    res.status(404).json({ error: 'Image file not found' });
-  }
-});
-app.get('/images/products/small/:year/:month/:day/:hour/:minute/:file', (req, res) => {
-  const { year, month, day, hour, minute, file } = req.params;
-  const imageFilePath = path.join(__dirname, `images/products/small/${year}/${month}/${day}/${hour}/${minute}/${file}`);
-
-  // Check if the image file exists
-  if (fs.existsSync(imageFilePath)) {
-    res.sendFile(imageFilePath);
-  } else {
-    res.status(404).json({ error: 'Image file not found' });
-  }
-});
-
-app.get('/images/products/medium/:year/:month/:day/:hour/:minute/:file', (req, res) => {
-  const { year, month, day, hour, minute, file } = req.params;
-  const imageFilePath = path.join(__dirname, `images/products/medium/${year}/${month}/${day}/${hour}/${minute}/${file}`);
-
-  // Check if the image file exists
-  if (fs.existsSync(imageFilePath)) {
-    res.sendFile(imageFilePath);
-  } else {
-    res.status(404).json({ error: 'Image file not found' });
-  }
-});
-
-app.get('/images/products/medium_small/:year/:month/:day/:hour/:minute/:file', (req, res) => {
-  const { year, month, day, hour, minute, file } = req.params;
-  const imageFilePath = path.join(__dirname, `images/products/medium_small/${year}/${month}/${day}/${hour}/${minute}/${file}`);
-
-  // Check if the image file exists
-  if (fs.existsSync(imageFilePath)) {
-    res.sendFile(imageFilePath);
-  } else {
-    res.status(404).json({ error: 'Image file not found' });
-  }
-});
-
-
-
-
-
-
   app.get('/', (req, res) => {
  
     res.send('Server running saklain mostak image')
